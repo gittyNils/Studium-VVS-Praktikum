b2f3c43f0f1c948a769a816726f683b950798023
Nils Schlicher <Nils.Schlicher@alumni.fh-aachen.de>
2019-04-09 14:06:45 +0200

    Commit nun auch vom Code statt nur die .gitignore

---
+++ chatone/chat.py
@@ -0,0 +1,13 @@
+import datetime
+print 'Content-Type: text/html'
+print ''
+print '<html>'
+print '<head>'
+print '<title>Welcome to Nils Schlicher\'s chat service</title>'
+print '</head>'
+print '<body>'
+print '<h1>Welcome to Nils Schlicher\'s chat service</h1>'
+print ''
+print 'Current time is: %s' % (datetime.datetime.now())
+print '</body>'
+print '</html>'
